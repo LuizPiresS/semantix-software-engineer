8106c54cd5a468d252ae9d42fc31da6289d55718
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-15 15:11:51 -0300

    feat: add validation to CreateCustomerDTO

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,1 +1,20 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateCustomerDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  phone: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  address: string;
+}
