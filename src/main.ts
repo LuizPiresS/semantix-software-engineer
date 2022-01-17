37c06fd39ddb146efaf3d2c4abf3ad74da19b367
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-17 13:10:41 +0000

    feat: add helmet and cors security layer

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Semantix API')
@@ -15,7 +17,6 @@ async function bootstrap() {
   SwaggerModule.setup('documentation', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
   await app.listen(3000);
 }
 
