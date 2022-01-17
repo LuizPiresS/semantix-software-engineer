8667eef40a1ffad2bcd5576d4df0819cd09a5693
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-17 12:15:51 +0000

    refactor: add pagination to findall method

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 
-@Controller('customer')
+@ApiTags('Customers')
+@Controller('customers')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiOperation({ summary: 'Create new customer' })
   @Post()
   create(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customerService.create(createCustomerDto);
   }
 
+  @ApiTags('Customers')
+  @ApiQuery({ name: 'page', required: false, type: Number })
+  @ApiQuery({ name: 'limit', required: false, type: Number })
+  @ApiOperation({ description: 'Find all customers' })
   @Get()
-  findAll() {
-    return this.customerService.findAll();
+  findAll(@Query('page') page = 1, @Query('limit') limit = 10) {
+    return this.customerService.findAll({ page, limit });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.customerService.findOne(+id);
+    return this.customerService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-    return this.customerService.update(+id, updateCustomerDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateCustomerDto: UpdateCustomerDto,
+  ) {
+    return this.customerService.update(id, updateCustomerDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.customerService.remove(+id);
+    return this.customerService.remove(id);
   }
 }
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -4,6 +4,11 @@ import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { Customer } from './entities/customer.entity';
 import { Repository } from 'typeorm';
+import {
+  IPaginationOptions,
+  Pagination,
+  paginate,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class CustomerService {
@@ -13,33 +18,65 @@ export class CustomerService {
     private customerRepository: Repository<Customer>,
   ) {}
   async create(createCustomerDto: CreateCustomerDto): Promise<Customer> {
-    if (
-      !createCustomerDto.name ||
-      !createCustomerDto.email ||
-      !createCustomerDto.password ||
-      !createCustomerDto.phoneNumber
-    ) {
-      throw new BadRequestException('Missing data');
-    }
+    try {
+      if (
+        !createCustomerDto.name ||
+        !createCustomerDto.email ||
+        !createCustomerDto.password ||
+        !createCustomerDto.phoneNumber
+      ) {
+        throw new BadRequestException('Missing data');
+      }
 
-    return this.customerRepository.save(
-      this.customerRepository.create(createCustomerDto),
-    );
+      return this.customerRepository.save(
+        this.customerRepository.create(createCustomerDto),
+      );
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error("Can't create customer");
+    }
   }
 
-  findAll() {
-    return this.customerRepository.find();
+  async findAll(options: IPaginationOptions): Promise<Pagination<Customer>> {
+    try {
+      const queryBuilder =
+        this.customerRepository.createQueryBuilder('customer');
+      queryBuilder.select(['customer.id', 'customer.name', 'customer.email']);
+
+      queryBuilder.orderBy('customer.name', 'ASC');
+
+      return await paginate<Customer>(queryBuilder, options);
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error("Can't find all customers");
+    }
   }
 
   async findOne(id: string): Promise<Customer> {
-    return await this.customerRepository.findOne(id);
+    try {
+      return await this.customerRepository.findOne(id);
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error("Can't find customer");
+    }
   }
 
-  update(id: string, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
+  async update(id: string, updateCustomerDto: UpdateCustomerDto) {
+    try {
+      return await this.customerRepository.update(id, updateCustomerDto);
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error("Can't update customer");
+    }
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} customer`;
+  async remove(id: string) {
+    try {
+      await this.customerRepository.delete(id);
+      return 'deleted';
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error("Can't delete customer");
+    }
   }
 }
