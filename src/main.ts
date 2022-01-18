ce13141f564369b1ded8e4f27124fe5556e21114
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-18 10:27:53 +0000

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import helmet from 'helmet';
+import { configService } from './config/config.service';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
@@ -16,8 +18,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
 
+  const logger = new Logger('Main');
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(3000);
+  logger.log(
+    `Application is running in -->production<-- mod: ${configService.isProduction()}`,
+  );
 }
 
 bootstrap();
