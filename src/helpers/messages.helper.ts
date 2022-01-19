b0733d45c46a526e55c5ae4c260de512323ca904
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-19 09:12:40 +0000

    refactor: move messages to helper files

---
+++ src/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessageHelper = {
+  INVALID_PASSWORD:
+    'Password must be at least 8 characters long and contain at least one number, one uppercase and one lowercase letter',
+  INVALID_CREDENTIALS: 'Invalid credentials',
+};
