56755afe9e7641e960af966eb6ac481672dc183c
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-20 12:45:12 +0000

    feat:  ensure return a product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
+  logger = new Logger(ProductsService.name);
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    this.logger.log('createProductDto', createProductDto);
+    return {
+      id: 'valid uuid',
+      name: 'valid name',
+      price: 1,
+      stock: 1,
+      createdAt: new Date().toDateString(),
+      updatedAt: new Date().toDateString(),
+      deletedAt: null,
+    };
   }
 
   findAll() {
