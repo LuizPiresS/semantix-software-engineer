f5e1816a42a181c4bf89ec83566b8fe73764128c
Luiz Jesuz <luiz.pires@ranzinza.dev>
2022-02-14 13:33:44 +0000

    refactor: remove unnecessary files

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,9 +1,4 @@
-import {
-  Body,
-  Injectable,
-  Logger,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { MessageHelper } from 'src/helpers/messages.helper';
